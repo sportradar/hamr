27400efaf2f199ed3373eb615583dca366b2162d
Gisle Martin Aune <dev@gisle.me>
2019-06-19 17:01:12 +0200

    add shellout::{save_data,load_data}

--- src/shellout.rs
+++ src/shellout.rs
@@ -3,6 +3,7 @@
 use std::error::Error;
 use std::process::{Command, Stdio};
 use std::fmt;
+use std::io::Write;
 
 fn exec_lpass(args: &[&str]) -> Result<String, LPassError> {
     match Command::new("lpass").args(args.iter()).output() {
@@ -16,6 +17,26 @@ fn exec_lpass(args: &[&str]) -> Result<String, LPassError> {
     }
 }
 
+fn exec_lpass_input(args: &[&str], data: &str) -> Result<String, LPassError> {
+    let mut child = Command::new("lpass").args(args.iter()).stdin(Stdio::piped()).spawn();
+    if let Err(err) = child {
+        return Err(LPassError::new("Could not exec command", Some(Box::from(err))));
+    }
+    let mut child = child.unwrap();
+
+    child.stdin.as_mut().unwrap().write_all(data.as_bytes());
+
+    match child.wait_with_output() {
+        Ok(out) => {
+            match String::from_utf8(out.stdout) {
+                Ok(out) => Ok(out),
+                Err(err) => Err(LPassError::new("Could not parse stdout as valid utf8", Some(Box::from(err)))),
+            }
+        },
+        Err(err) => Err(LPassError::new("Could not exec command", Some(Box::from(err))))
+    }
+}
+
 fn exec_lpass_no_output(args: &[&str]) -> Result<(), LPassError> {
     match Command::new("lpass").args(args.iter()).stdout(Stdio::inherit()).stderr(Stdio::inherit()).output() {
         Ok(_) => Ok(()),
@@ -23,8 +44,44 @@ fn exec_lpass_no_output(args: &[&str]) -> Result<(), LPassError> {
     }
 }
 
+pub fn save_data(name: &str, data: &str) -> Result<(), LPassError> {
+    let mut note_data= String::with_capacity(1024);
+    note_data.push_str("HamrData: ");
+    note_data.push_str(data);
+
+    exec_lpass_input(&["edit", name, "--non-interactive", "--sync=now"], &note_data)?;
+
+    Ok(())
+}
+
+pub fn load_data(name: &str) -> Result<String, LPassError> {
+    let mut output = exec_lpass(&["show", name])?;
+
+    // Remove the header line.
+    if let Some((i, _)) = output.chars().enumerate().find(|(i, s)| *s == '\n') {
+        if output.len() > i + 1 {
+            output.drain(..i+1);
+        } else {
+            output.clear();
+        }
+    }
+
+    // Clean it up.
+    if output.starts_with("HamrData: ") {
+        output.drain(..10);
+    }
+    if output.ends_with('\n') {
+        output.pop();
+    }
+    if output.ends_with('\r') {
+        output.pop();
+    }
+
+    Ok(output)
+}
+
 pub fn login(username: &str) -> Result<(), LPassError> {
-    return exec_lpass_no_output(&["login", username])
+    exec_lpass_no_output(&["login", username])
 }
 
 pub fn ls() -> Result<Vec<LSEntry>, LPassError> {
