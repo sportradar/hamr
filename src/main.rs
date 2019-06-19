a7ad4945d158be48322830aa8999b23eed1610e7
kollstrom <e.kollstrom@gmail.com>
2019-06-19 14:28:20 +0200

    Add login ui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod shellout;
 
-fn main() {
+use structopt::StructOpt;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
 
+#[derive(StructOpt)]
+#[structopt(name = "hamr", about = "The build tool that let's you create, share and import secrets with your Git repositories easily via LastPass")]
+enum Hamr {
+    #[structopt(name = "login")]
+    Login {
+        username: String,
+    }
+}
+
+fn main() {
+    let args = Hamr::from_args();
 }
