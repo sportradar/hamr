09af0d71640d77470568ca2e4de0f15612f43aa6
kollstrom <e.kollstrom@gmail.com>
2019-06-19 14:46:41 +0200

    Add doc for login

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use exitfailure::ExitFailure;
 #[derive(StructOpt)]
 #[structopt(name = "hamr", about = "The build tool that let's you create, share and import secrets with your Git repositories easily via LastPass")]
 enum Hamr {
-    #[structopt(name = "login")]
+    #[structopt(name = "login", about="Logs in to LastPass")]
     Login {
         username: String,
     }
